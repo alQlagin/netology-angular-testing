0c656640ef91e20540b5a211033f7aeafd2b76fc
Alex Kulagin <al.qlagin@gmail.com>
2017-05-21 19:09:53 +0500

    sinon example in unit test

--- test/unit/controller.spec.js
+++ test/unit/controller.spec.js
@@ -1,17 +1,14 @@
 chai.should();
 describe("PhoneListController", () => {
+    let ctrl;
+    let stub;
     beforeEach(module("app"));
-    it('should use PhonesService', inject(function ($controller) {
-        let callCounter = 0;
-        var PhonesService = {
-            add: function () {
-                callCounter++;
-            }
-        };
-        var ctrl = $controller('PhoneListController', {PhonesService});
-
+    beforeEach(inject(function ($controller, PhonesService) {
+        ctrl = $controller('PhoneListController', {PhonesService});
+        stub = sinon.stub(PhonesService, 'add')
+    }));
+    it('should use PhonesService', inject(function ($controller, PhonesService) {
         ctrl.add("Sony");
-        ctrl.add("Motorolla");
-        callCounter.should.be.equal(2);
+        sinon.assert.calledOnce(stub)
     }));
 });
